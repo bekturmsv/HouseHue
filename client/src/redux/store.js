076ed87118c1744472c044dbe4119aa40cb6c8e6
bekturmsv <itsbektur@gmail.com>
2023-10-27 01:27:07 +0600

    install and create user slice (redux)

---
+++ client/src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice";
+
+export const store = configureStore({
+  reducer: { user: userReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
