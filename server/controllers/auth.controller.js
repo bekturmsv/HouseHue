ce84c58eda4fea02531b214f700242016837f5b8
bekturmsv <itsbektur@gmail.com>
2023-10-27 20:15:58 +0600

    add Google auth functionality

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -34,3 +34,40 @@ export const signin = async (req, res, next) => {
     next(error);
   }
 };
+
+export const googleAuth = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const { password: pass, ...userInfo } = user._doc;
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(userInfo);
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+
+      const hashedPassword = bcrypt.hashSync(generatedPassword, 10);
+      const newUser = new User({
+        username:
+          req.body.name.split(" ").join("").toLowerCase() +
+          Math.random().toString(36).slice(-4),
+        email: req.body.email,
+        password: hashedPassword,
+        avatar: req.body.photo,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password: pass, userInfo } = newUser._doc;
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(userInfo);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { signin, signup } from "../controllers/auth.controller.js";
+import { googleAuth, signin, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
-
+router.post("/google", googleAuth);
 export default router;
