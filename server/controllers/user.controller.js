d0db5016f32fc723d5fb9276a379b01f85a65849
bekturmsv <itsbektur@gmail.com>
2023-10-29 17:48:04 +0600

    create update user API route

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,36 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcrypt from "bcrypt";
+
 export const test = (req, res) => {
   res.json({
     message: "Hello World",
   });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only update your own account!"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcrypt.hashSync(req.body.password, 10);
+    }
+
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...userInfo } = updateUser._doc;
+    res.status(200).json(userInfo);
+  } catch (error) {
+    next(error);
+  }
 };
