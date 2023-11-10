37a6edd37e3a94665fb408aae584c0037e62b681
bekturmsv <itsbektur@gmail.com>
2023-11-10 21:10:04 +0600

    change title

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,8 +10,8 @@ function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to={"/"}>
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">MERN</span>
-            <span className="text-slate-700">Estate</span>
+            <span className="text-slate-500">HOUSE </span>
+            <span className="text-slate-700"> HUE</span>
           </h1>
         </Link>
         <form
