136cda91c6641fd630279106d450a9b8c386de60
bekturmsv <itsbektur@gmail.com>
2023-11-21 00:54:35 +0600

    add swiper section for the Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/bundle";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
 
 const Home = () => {
+  const [offerListings, setOfferListings] = useState([]);
+  const [saleListings, setSaleListings] = useState([]);
+  const [rentListings, setRentListings] = useState([]);
+  SwiperCore.use([Navigation]);
+  console.log(offerListings);
+  useEffect(() => {
+    const fetchOfferListings = async () => {
+      try {
+        const res = await fetch(
+          `/api/listing/getListings?offer=true&limit=4`
+        ).then((res) => res.json());
+        setOfferListings(res);
+        fetchRentListings();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchRentListings = async () => {
+      try {
+        const res = await fetch(
+          `/api/listing/getListings?type=rent&limit=4`
+        ).then((res) => res.json());
+        setRentListings(res);
+        fetchSaleListings();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchSaleListings = async () => {
+      try {
+        const res = await fetch(
+          "/api/listing/getListings?type=sale&limit=4"
+        ).then((res) => res.json());
+        setSaleListings(res);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchOfferListings();
+  }, []);
   return (
     <div>
       {/* top */}
@@ -23,7 +69,21 @@ const Home = () => {
         </Link>
       </div>
       {/* swiper */}
-
+      <Swiper navigation>
+        {offerListings &&
+          offerListings.length > 0 &&
+          offerListings.map((listing) => (
+            <SwiperSlide key={listing._id}>
+              <div
+                style={{
+                  background: `url(${listing.imageUrls[0]}) center no-repeat`,
+                  backgroundSize: "cover",
+                }}
+                className="h-[500px]"
+              ></div>
+            </SwiperSlide>
+          ))}
+      </Swiper>
       {/* listing results for offer, sale and rent */}
     </div>
   );
